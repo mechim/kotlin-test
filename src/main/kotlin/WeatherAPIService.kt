391b245efae705fab54bd2deb8340083c409ca9f
mechim <michael.echim@gmail.com>
2025-09-09 16:52:38 +0300

    add query parameters, make data models

--- src/main/kotlin/WeatherAPIService.kt
+++ src/main/kotlin/WeatherAPIService.kt
@@ -1,6 +1,15 @@
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface WeatherAPIService {
     @GET("/forecast.json")
-    suspend fun getForecast(): WeatherData
+    suspend fun getForecast(
+        @Query("q") city: String,
+        @Query("days") days: Int=1,
+        @Query("dt") date: String,
+        @Query("alerts") alerts: String = "no",
+        @Query("aqi") aqi: String = "no",
+        @Query("hour") hour: Int = 12, //don't need all 24 hours in the response
+        @Query("key") key: String,
+    ): WeatherData
 }
--- src/main/kotlin/WeatherData.kt
+++ src/main/kotlin/WeatherData.kt
@@ -1,6 +1,29 @@
+import com.squareup.moshi.Json
+
+// Root
 data class WeatherData(
-    var location: String,
-    var current: String,
-    var forecast: String,
-    var alerts: String,
+    val forecast: Forecast?
+)
+
+// --- forecast section ---
+data class Forecast(
+    val forecastday: List<ForecastDay>
+)
+
+data class ForecastDay(
+    val day: Day,
+    val hour: List<Hour>
+)
+
+// --- day-level info ---
+data class Day(
+    @Json(name = "maxtemp_c") val maxTempC: Double,
+    @Json(name = "mintemp_c") val minTempC: Double,
+    @Json(name = "avghumidity") val avgHumidity: Int,
+    @Json(name = "maxwind_kph") val maxWindKph: Double
+)
+
+// --- hour-level info ---
+data class Hour(
+    @Json(name = "wind_dir") val windDir: String
 )
